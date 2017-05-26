e8e3a1d9a2dceb64a0cff7201c7fe0d3e66c6231
Chris Carr <coder.knot@gmail.com>
2017-05-26 11:24:15 -0700

    implement View.OnClickListener interface for handling on click event of multiple views

--- app/src/main/java/com/epicodus/smallsteps/MainActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/MainActivity.java
@@ -3,13 +3,18 @@ package com.epicodus.smallsteps;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.beginButton) Button mBeginButton;
+    @Bind(R.id.aboutButton) Button mAboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         Typeface appNameFont = Typeface.createFromAsset(getAssets(), "fonts/app_name.ttf");
         mAppNameTextView.setTypeface(appNameFont);
+
+        mBeginButton.setOnClickListener(this);
+        mAboutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mBeginButton) {
+            Toast.makeText(MainActivity.this, "Begin button clicked", Toast.LENGTH_LONG).show();
+        }
+        if(v == mAboutButton) {
+            Toast.makeText(MainActivity.this, "About button clicked", Toast.LENGTH_LONG).show();
+        }
     }
 }
