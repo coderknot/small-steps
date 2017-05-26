677ef4802ac1b79c75e50c8e69ae12a5622e8a49
Chris Carr <coder.knot@gmail.com>
2017-05-26 16:43:40 -0700

    implement View.OnClickListener interface and on click event handler for back button

--- app/src/main/java/com/epicodus/smallsteps/AboutActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/AboutActivity.java
@@ -1,17 +1,31 @@
 package com.epicodus.smallsteps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.backButton) Button mBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
+
+        mBackButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mBackButton) {
+            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
