009a648be24b499bbeebfb46e3c601ceda8fb464
Chris Carr <coder.knot@gmail.com>
2017-05-26 15:16:00 -0700

    add imports for ButterKnife

--- app/src/main/java/com/epicodus/smallsteps/AboutActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/AboutActivity.java
@@ -3,11 +3,15 @@ package com.epicodus.smallsteps;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
     }
 }
