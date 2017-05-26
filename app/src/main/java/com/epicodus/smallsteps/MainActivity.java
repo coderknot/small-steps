04029d5af1ec3ff2f8aac97f74858ccf2fe31a8a
Chris Carr <coder.knot@gmail.com>
2017-05-26 10:44:20 -0700

    set font of app name text view to app name font

--- app/src/main/java/com/epicodus/smallsteps/MainActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/MainActivity.java
@@ -1,13 +1,23 @@
 package com.epicodus.smallsteps;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface appNameFont = Typeface.createFromAsset(getAssets(), "fonts/app_name.ttf");
+        mAppNameTextView.setTypeface(appNameFont);
     }
 }
