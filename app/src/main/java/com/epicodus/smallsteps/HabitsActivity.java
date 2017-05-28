566c769846a54ca8a4f2a585cce0b48e49b50ee8
Chris Carr <coder.knot@gmail.com>
2017-05-27 18:00:31 -0700

    add sample array of habits and functionality to display list in habits list view

--- app/src/main/java/com/epicodus/smallsteps/HabitsActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/HabitsActivity.java
@@ -2,16 +2,27 @@ package com.epicodus.smallsteps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class HabitsActivity extends AppCompatActivity {
+    private String[] habits = new String[] {"Brush Teeth", "Study", "Exercise", "Read", "Eat", "Wash Dishes", "Meditate"};
+    private ListView mHabitsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habits);
         ButterKnife.bind(this);
+
+        mHabitsListView = (ListView) findViewById(R.id.habitsListView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, habits);
+
+        mHabitsListView.setAdapter(adapter);
     }
 }
