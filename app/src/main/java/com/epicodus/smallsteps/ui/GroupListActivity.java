be4a13843eb61278ead481da65439a1dfed5d3af
Chris Carr <coder.knot@gmail.com>
2017-06-03 13:49:18 -0700

    add method to call meetup group service to return group list

--- app/src/main/java/com/epicodus/smallsteps/ui/GroupListActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/GroupListActivity.java
@@ -7,12 +7,22 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.epicodus.smallsteps.R;
+import com.epicodus.smallsteps.models.Group;
+import com.epicodus.smallsteps.services.MeetupGroupService;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class GroupListActivity extends AppCompatActivity {
     @Bind(R.id.groupRecyclerView) RecyclerView mGroupRecyclerView;
+
+    public ArrayList<Group> mGroupList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,23 @@ public class GroupListActivity extends AppCompatActivity {
 
         Log.v("Group List Activity", searchText);
         Log.v("Group List Activity", searchZip);
+
+        getGroupList(searchText, searchZip);
+    }
+
+    private void getGroupList(String searchText, String searchZip) {
+        final MeetupGroupService meetupGroupService = new MeetupGroupService();
+        meetupGroupService.getGroups(searchText, searchZip, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mGroupList = meetupGroupService.processResults(response);
+            }
+        });
     }
 }
