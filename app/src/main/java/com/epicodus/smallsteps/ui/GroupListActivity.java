bf16c48dd20faa0b12a239aa3713eb813252646a
Chris Carr <coder.knot@gmail.com>
2017-06-03 11:36:39 -0700

    create and add functionality to capture search data from search activity

--- app/src/main/java/com/epicodus/smallsteps/ui/GroupListActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/GroupListActivity.java
@@ -1,15 +1,30 @@
 package com.epicodus.smallsteps.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.epicodus.smallsteps.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class GroupListActivity extends AppCompatActivity {
+    @Bind(R.id.groupRecyclerView) RecyclerView mGroupRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_list);
+        ButterKnife.bind(this);
+
+        Intent searchIntent = getIntent();
+        String searchText = searchIntent.getStringExtra("searchText");
+        String searchZip = searchIntent.getStringExtra("searchZip");
+
+        Log.v("Group List Activity", searchText);
+        Log.v("Group List Activity", searchZip);
     }
 }
