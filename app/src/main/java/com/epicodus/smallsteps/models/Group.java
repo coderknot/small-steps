65b9e17c3f6d16d04a42c542a99c11e24b21ecef
Chris Carr <coder.knot@gmail.com>
2017-06-03 13:47:12 -0700

    create group model

--- app/src/main/java/com/epicodus/smallsteps/models/Group.java
+++ app/src/main/java/com/epicodus/smallsteps/models/Group.java
@@ -1,8 +1,64 @@
 package com.epicodus.smallsteps.models;
 
-/**
- * Created by chris on 6/3/17.
- */
+import com.epicodus.smallsteps.ui.SearchActivity;
 
 public class Group {
+    String name; //name
+    String link; //link
+    String category; //category: name
+    String city; //city
+    String state; //state
+    String country; //country
+    String description; //description
+    int memberCount; //members
+
+    public Group(String name,
+                 String link,
+                 String category,
+                 String city,
+                 String state,
+                 String country,
+                 String description,
+                 int memberCount) {
+        this.name = name;
+        this.link = link;
+        this.category = category;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.description = description;
+        this.memberCount = memberCount;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLink() {
+        return this.link;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getMemberCount() {
+        return this.memberCount;
+    }
 }
