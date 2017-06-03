652c9f0026a03d7f407c063f147fc4d4e6aca2ef
Chris Carr <coder.knot@gmail.com>
2017-06-03 13:46:23 -0700

    add class for storing API constants for building url query

--- app/src/main/java/com/epicodus/smallsteps/Constants.java
+++ app/src/main/java/com/epicodus/smallsteps/Constants.java
@@ -1,8 +1,11 @@
 package com.epicodus.smallsteps;
 
-/**
- * Created by chris on 6/3/17.
- */
-
 public class Constants {
+    public static final String MEETUP_API_KEY = BuildConfig.MEETUP_API_KEY;
+    public static final String MEETUP_BASE_URL = "https://api.meetup.com/find/groups?";
+    public static final String MEETUP_SIGN_PARAMETER = "sign";
+    public static final String MEETUP_PHOTO_HOST_PARAMETER = "photo-host";
+    public static final String MEETUP_ZIP_PARAMETER = "zip";
+    public static final String MEETUP_TEXT_PARAMETER = "text";
+    public static final String MEETUP_API_QUERY_PARAMETER = "key";
 }
