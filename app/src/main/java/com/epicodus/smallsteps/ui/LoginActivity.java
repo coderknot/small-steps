3d12cf1566dce113c85fe6caa111b061b0e90b6e
Chris Carr <coder.knot@gmail.com>
2017-06-08 14:40:37 -0700

    create on click to navigate to registration activity

--- app/src/main/java/com/epicodus/smallsteps/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.epicodus.smallsteps.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.smallsteps.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.loginRegistrationTextView) TextView mRegistrationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mRegistrationTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mRegistrationTextView) {
+            Intent registrationIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
+            startActivity(registrationIntent);
+        }
     }
 }
