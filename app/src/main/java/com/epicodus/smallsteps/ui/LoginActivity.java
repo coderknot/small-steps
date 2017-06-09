84b829289040cfe9ba8d1bd32300095e66c1a77f
Chris Carr <coder.knot@gmail.com>
2017-06-08 17:45:31 -0700

    add login functionality

--- app/src/main/java/com/epicodus/smallsteps/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/LoginActivity.java
@@ -1,18 +1,36 @@
 package com.epicodus.smallsteps.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.smallsteps.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @Bind(R.id.loginRegistrationTextView) TextView mRegistrationTextView;
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Bind(R.id.loginEmailEditText) EditText mLoginEmailEditText;
+    @Bind(R.id.loginPasswordEditText) EditText mLoginPasswordEditText;
+    @Bind(R.id.loginLoginButton) Button mLoginButton;
+    @Bind(R.id.loginRegistrationTextView) TextView mLoginRegistrationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +38,90 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-        mRegistrationTextView.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+
+        mLoginButton.setOnClickListener(this);
+        mLoginRegistrationTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        if(v == mRegistrationTextView) {
+        if(v == mLoginButton) {
+            loginUser();
+        }
+        if(v == mLoginRegistrationTextView) {
             Intent registrationIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
             startActivity(registrationIntent);
         }
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null) {
+                    Intent authStateIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    authStateIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(authStateIntent);
+                }
+            }
+        };
+    }
+
+    private void loginUser() {
+        String email = mLoginEmailEditText.getText().toString().trim();
+        String password = mLoginPasswordEditText.getText().toString().trim();
+
+        if(!isValidEmail(email) || !isValidPassword(password)) return;
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mLoginEmailEditText.setError("Please enter a valid email address.");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidPassword(String password) {
+        if (password.length() < 6) {
+            mLoginPasswordEditText.setError("Please enter a valid password.");
+            return false;
+        }
+        return true;
     }
 }
