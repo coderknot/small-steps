c55ffa3dc12521c2651d1714563564027398a1fc
Chris Carr <coder.knot@gmail.com>
2017-06-10 12:29:29 -0700

    add user id property to habit model

--- app/src/main/java/com/epicodus/smallsteps/models/Habit.java
+++ app/src/main/java/com/epicodus/smallsteps/models/Habit.java
@@ -5,6 +5,7 @@ import org.parceler.Parcel;
 @Parcel
 public class Habit {
     String title;
+    String uid;
 
     public Habit() { }
 
@@ -14,5 +15,13 @@ public class Habit {
 
     public String getTitle() {
         return this.title;
+    }
+
+    public String getUid() {
+        return this.uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 }
