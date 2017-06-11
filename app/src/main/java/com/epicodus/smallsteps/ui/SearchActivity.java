444ddf04cbb28e6ca76363b8934a37907ffaef17
Chris Carr <coder.knot@gmail.com>
2017-06-11 14:25:27 -0700

    add functionality to saved location to shared preferences and autofill location edit text if saved

--- app/src/main/java/com/epicodus/smallsteps/ui/SearchActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.epicodus.smallsteps.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.epicodus.smallsteps.Constants;
 import com.epicodus.smallsteps.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
+    private SharedPreferences mLocationSharedPreferences;
+    private SharedPreferences.Editor mLocationEditor;
+
     @Bind(R.id.searchButton) Button mSearchButton;
     @Bind(R.id.searchEditText) EditText mSearchEditText;
     @Bind(R.id.zipEditText) EditText mZipEditText;
@@ -24,6 +30,11 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
 
+        mLocationSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mLocationEditor = mLocationSharedPreferences.edit();
+
+        determineLocationDisplay();
+
         mSearchButton.setOnClickListener(this);
     }
 
@@ -33,8 +44,9 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             String searchText = mSearchEditText.getText().toString().toLowerCase();
             String searchZip = mZipEditText.getText().toString();
 
-            Log.v("Search Activity", searchText);
-            Log.v("Search Activity", searchZip);
+            if(!(isLocationSaved())) {
+                addToSharedPreferences(searchZip);
+            }
 
             Intent searchIntent = new Intent(SearchActivity.this, GroupListActivity.class);
             searchIntent.putExtra("searchText", searchText);
@@ -42,4 +54,40 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(searchIntent);
         }
     }
+
+    private void determineLocationDisplay() {
+        if(isLocationSaved()) {
+            String savedLocation = mLocationSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+            setLocationDisplay(savedLocation);
+        }
+    }
+
+    private void setLocationDisplay(String location) {
+        mZipEditText.setText(location);
+    }
+
+    private void addToSharedPreferences(String location) {
+        mLocationEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    }
+
+    private boolean isLocationSaved() {
+        String location = mLocationSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+
+        if(location == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean matchesSavedLocation(String location) {
+        String savedLocation = mLocationSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+
+        if(savedLocation.equals(location)) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
