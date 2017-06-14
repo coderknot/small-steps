a9882ab349ce939137f78361b23ffb9adbcb5bab
Chris Carr <coder.knot@gmail.com>
2017-06-14 14:59:53 -0700

    update to use new adapter and utilized movement functionality

--- app/src/main/java/com/epicodus/smallsteps/ui/HabitsListActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/HabitsListActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.Button;
 
 import com.epicodus.smallsteps.Constants;
 import com.epicodus.smallsteps.R;
+import com.epicodus.smallsteps.adapters.FirebaseHabitListAdapter;
 import com.epicodus.smallsteps.adapters.FirebaseHabitViewHolder;
+import com.epicodus.smallsteps.util.ItemTouchHelperAdapter;
+import com.epicodus.smallsteps.util.OnStartDragListener;
+import com.epicodus.smallsteps.util.SimpleItemTouchHelperCallback;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,9 +27,10 @@ import com.epicodus.smallsteps.models.Habit;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class HabitsListActivity extends AppCompatActivity implements View.OnClickListener {
+public class HabitsListActivity extends AppCompatActivity implements OnStartDragListener, View.OnClickListener {
     private DatabaseReference mHabitReference;
-    private FirebaseRecyclerAdapter mFirebaseRecyclerAdapter;
+    private FirebaseHabitListAdapter mFirebaseHabitListAdapter;
+    private ItemTouchHelper mItemTouchHolder;
 
     @Bind(R.id.habitsRecyclerView) RecyclerView mHabitsRecyclerView;
     @Bind(R.id.habitsListNewHabitButton) Button mHabitsListNewHabitButton;
@@ -35,6 +41,12 @@ public class HabitsListActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_habits_list);
         ButterKnife.bind(this);
 
+        setUpFirebaseRecyclerAdapter();
+
+        mHabitsListNewHabitButton.setOnClickListener(this);
+    }
+
+    private void setUpFirebaseRecyclerAdapter() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
 
@@ -43,23 +55,20 @@ public class HabitsListActivity extends AppCompatActivity implements View.OnClic
                 .getReference(Constants.FIREBASE_CHILD_HABITS)
                 .child(uid);
 
-        setUpFirebaseRecyclerAdapter();
-
-        mHabitsListNewHabitButton.setOnClickListener(this);
-    }
-
-    private void setUpFirebaseRecyclerAdapter() {
-        mFirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Habit, FirebaseHabitViewHolder>
-                (Habit.class, R.layout.habit_list_item, FirebaseHabitViewHolder.class, mHabitReference) {
-            @Override
-            public void populateViewHolder(FirebaseHabitViewHolder habitViewHolder, Habit habit, int position) {
-                habitViewHolder.bindHabit(habit);
-            }
-        };
+        mFirebaseHabitListAdapter = new FirebaseHabitListAdapter(Habit.class,
+                                                                 R.layout.habit_list_item,
+                                                                 FirebaseHabitViewHolder.class,
+                                                                 mHabitReference,
+                                                                 this,
+                                                                 this);
 
         mHabitsRecyclerView.setHasFixedSize(true);
         mHabitsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mHabitsRecyclerView.setAdapter(mFirebaseRecyclerAdapter);
+        mHabitsRecyclerView.setAdapter(mFirebaseHabitListAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseHabitListAdapter);
+        mItemTouchHolder = new ItemTouchHelper(callback);
+        mItemTouchHolder.attachToRecyclerView(mHabitsRecyclerView);
     }
 
     @Override
@@ -71,8 +80,13 @@ public class HabitsListActivity extends AppCompatActivity implements View.OnClic
     }
 
     @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHolder.startDrag(viewHolder);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        mFirebaseRecyclerAdapter.cleanup();
+        mFirebaseHabitListAdapter.cleanup();
     }
 }
