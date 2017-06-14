4c78b16d32e71d5e197d99698fd59f147c684c72
Chris Carr <coder.knot@gmail.com>
2017-06-14 15:32:00 -0700

    create logic to swap moved habit items, remove individual habits, and keep habit index updated in firebase

--- app/src/main/java/com/epicodus/smallsteps/adapters/FirebaseHabitListAdapter.java
+++ app/src/main/java/com/epicodus/smallsteps/adapters/FirebaseHabitListAdapter.java
@@ -9,13 +9,21 @@ import com.epicodus.smallsteps.models.Habit;
 import com.epicodus.smallsteps.util.ItemTouchHelperAdapter;
 import com.epicodus.smallsteps.util.OnStartDragListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FirebaseHabitListAdapter extends FirebaseRecyclerAdapter<Habit, FirebaseHabitViewHolder> implements ItemTouchHelperAdapter {
     private DatabaseReference mRef;
     private OnStartDragListener mOnStartDragListener;
     private Context mContext;
+    private ChildEventListener mChildEventListener;
+    private ArrayList<Habit> mHabits = new ArrayList<>();
 
     public FirebaseHabitListAdapter(Class<Habit> modelClass,
                                     int modelLayout,
@@ -27,6 +35,33 @@ public class FirebaseHabitListAdapter extends FirebaseRecyclerAdapter<Habit, Fir
         this.mRef = ref.getRef();
         this.mOnStartDragListener = onStartDragListener;
         this.mContext = context;
+
+        mChildEventListener = mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                mHabits.add(dataSnapshot.getValue(Habit.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -46,11 +81,30 @@ public class FirebaseHabitListAdapter extends FirebaseRecyclerAdapter<Habit, Fir
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
+        Collections.swap(mHabits, fromPosition, toPosition);
+        notifyItemMoved(fromPosition, toPosition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
+        mHabits.remove(position);
+        getRef(position).removeValue();
+    }
 
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        setIndexInFirebase();
+        mRef.removeEventListener(mChildEventListener);
+    }
+
+    private void setIndexInFirebase() {
+        for(Habit habit : mHabits) {
+            int index = mHabits.indexOf(habit);
+            DatabaseReference ref = getRef(index);
+            habit.setIndex(Integer.toString(index));
+            ref.setValue(habit);
+        }
     }
 }
