1f099030e11fc4ae0c2295f999b4990bbf80d970
Chris Carr <coder.knot@gmail.com>
2017-06-13 18:44:35 -0700

    remove unused imports

--- app/src/main/java/com/epicodus/smallsteps/ui/HabitsListActivity.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/HabitsListActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,13 +14,10 @@ import com.epicodus.smallsteps.adapters.FirebaseHabitViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import com.epicodus.smallsteps.models.Habit;
-import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
