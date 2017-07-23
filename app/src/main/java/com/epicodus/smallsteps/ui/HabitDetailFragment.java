5f634bc92bc23cd016289a1fdddf199c2b5e3f87
Chris Carr <coder.knot@gmail.com>
2017-07-23 16:47:33 -0700

    verify clicked habit in habits list passed successfully

--- app/src/main/java/com/epicodus/smallsteps/ui/HabitDetailFragment.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/HabitDetailFragment.java
@@ -5,14 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.epicodus.smallsteps.R;
+import com.epicodus.smallsteps.models.Habit;
+
+import org.parceler.Parcels;
 
 public class HabitDetailFragment extends Fragment {
-
+    Habit habit;
 
     public HabitDetailFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        habit = Parcels.unwrap(getArguments().getParcelable("Habit"));
     }
 
 
@@ -23,6 +33,9 @@ public class HabitDetailFragment extends Fragment {
         if(getActivity() instanceof Main2Activity) {
             ((Main2Activity) getActivity()).hideFloatingActionButton();
         }
+
+        Toast.makeText(getActivity(), "Passed Habit: " + habit.getTitle(), Toast.LENGTH_SHORT).show();
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_habit_detail, container, false);
