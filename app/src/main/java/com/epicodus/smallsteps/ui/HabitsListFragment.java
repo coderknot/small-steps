976e401a09300920a9e1f0cc0118c92359f458da
Chris Carr <coder.knot@gmail.com>
2017-07-23 14:48:25 -0700

    update to use generic FirebaseRecyclerAdapter with custom FirebaseHabitViewHolder

--- app/src/main/java/com/epicodus/smallsteps/ui/HabitsListFragment.java
+++ app/src/main/java/com/epicodus/smallsteps/ui/HabitsListFragment.java
@@ -2,15 +2,16 @@ package com.epicodus.smallsteps.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.epicodus.smallsteps.Constants;
 import com.epicodus.smallsteps.R;
-import com.epicodus.smallsteps.adapters.FirebaseHabitListAdapter;
 import com.epicodus.smallsteps.adapters.FirebaseHabitViewHolder;
 import com.epicodus.smallsteps.models.Habit;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -22,10 +23,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class HabitsListFragment extends Fragment /*implements View.OnClickListener*/ {
+public class HabitsListFragment extends Fragment {
     private DatabaseReference mHabitReference;
-    private FirebaseRecyclerAdapter mFirebaseRecyclerAdapter;
-    private FirebaseHabitListAdapter mFirebaseHabitListAdapter;
 
     @Bind(R.id.habitsRecyclerView) RecyclerView mHabitsRecyclerView;
 
@@ -53,32 +52,37 @@ public class HabitsListFragment extends Fragment /*implements View.OnClickListen
                 .getReference(Constants.FIREBASE_CHILD_HABITS)
                 .child(uid);
 
-        mFirebaseHabitListAdapter = new FirebaseHabitListAdapter(Habit.class, R.layout.habit_list_item, FirebaseHabitViewHolder.class, mHabitReference);
+        FirebaseRecyclerAdapter<Habit, FirebaseHabitViewHolder> adapter = new FirebaseRecyclerAdapter<Habit, FirebaseHabitViewHolder>(Habit.class, R.layout.habit_list_item, FirebaseHabitViewHolder.class, mHabitReference) {
+            @Override
+            protected void populateViewHolder(FirebaseHabitViewHolder viewHolder, Habit model, int position) {
+                viewHolder.habitTitleTextView.setText(model.getTitle());
+            }
 
-//        mFirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Habit, FirebaseHabitViewHolder>
-//                (Habit.class, R.layout.habit_list_item, FirebaseHabitViewHolder.class, mHabitReference) {
-//            @Override
-//            public void populateViewHolder(FirebaseHabitViewHolder habitViewHolder, Habit habit, int position) {
-//                habitViewHolder.bindHabit(habit);
-//            }
-//        };
+            @Override
+            public FirebaseHabitViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                FirebaseHabitViewHolder firebaseHabitViewHolder = super.onCreateViewHolder(parent, viewType);
+                firebaseHabitViewHolder.setOnClickListener(new FirebaseHabitViewHolder.ClickListener() {
+                    @Override
+                    public void onItemClick(View v, int position) {
+                        Toast.makeText(getActivity(), "Item clicked at " + position, Toast.LENGTH_SHORT).show();
+
+                        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.content_frame, new HabitDetailFragment());
+                        fragmentTransaction.commit();
+                    }
+                });
+                return firebaseHabitViewHolder;
+            }
+        };
 
         mHabitsRecyclerView.setHasFixedSize(true);
         mHabitsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        mHabitsRecyclerView.setAdapter(mFirebaseRecyclerAdapter);
-        mHabitsRecyclerView.setAdapter(mFirebaseHabitListAdapter);
+        mHabitsRecyclerView.setAdapter(adapter);
     }
-
-//    @Override
-//    public void onClick(View v) {
-//
-//    }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-//        mFirebaseRecyclerAdapter.cleanup();
-        mFirebaseHabitListAdapter.cleanup();
     }
 
 }
