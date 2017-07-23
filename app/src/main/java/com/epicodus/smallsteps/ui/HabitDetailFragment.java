0b9dad510f4696676307f9ab6462ca015d0038f9
Chris Carr <coder.knot@gmail.com>
2017-07-23 14:48:45 -0700

    create habit detail fragment

---
+++ app/src/main/java/com/epicodus/smallsteps/ui/HabitDetailFragment.java
@@ -0,0 +1,26 @@
+package com.epicodus.smallsteps.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.smallsteps.R;
+
+public class HabitDetailFragment extends Fragment {
+
+
+    public HabitDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_habit_detail, container, false);
+    }
+
+}
